c98ba60bffa95b17cb4b3b355bc4f527e5433b7d
brian <brian@toimc.com>
2021-10-14 10:30:13 +0800

    feat: [资源] 删除语言文件

--- locales/es/crd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'Custom Resource Definition': 'Custom Resource Definition',
-  'Custom Resource Definitions': 'Custom Resource Definitions',
-  CUSTOM_RESOURCE_DEFINITION_DESC:
-    'A Custom Resource Definition (CRD) allows users to create a new type of resources without adding another API server. They can use these resources like any other native Kubernetes objects.',
-  CUSTOM_RESOURCE_DEFINITION_CREATE_DESC:
-    'A Custom Resource Definition (CRD) allows users to create a new type of resources without adding another API server. They can use these resources like any other native Kubernetes objects.',
-}
--- locales/tc/crd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'Custom Resource Definition': '自定義資源 (CRD)',
-  'Custom Resource Definitions': '自定義資源 (CRD)',
-
-  CUSTOM_RESOURCE_DEFINITION_DESC:
-    '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。',
-  CUSTOM_RESOURCE_DEFINITION_CREATE_DESC:
-    '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。',
-}
