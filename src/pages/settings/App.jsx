250ba47ae73cb79ccbf3019f8839ba5e258dd9b2
brian <brian@toimc.com>
2021-10-21 22:15:54 +0800

    perf: [UI] 删除不用的pages

--- src/pages/settings/App.jsx
+++
@@ -1,8 +0,0 @@
-import { inject } from 'mobx-react'
-import { renderRoutes } from 'utils/router.config'
-
-import routes from './routes'
-
-const App = () => renderRoutes(routes)
-
-export default inject('rootStore')(App)
